66bfc1ba524f5c12fe629e2c513558805520101d
jrp2014 <jrp2014@users.noreply.github.com>
2019-12-08 18:52:54 +0000

    Day 7, part 2

--- Day07/Day07.hs
+++ Day07/Day07.hs
@@ -1,29 +1,31 @@
-module Day05 where
+module Day07 where
 
-import qualified Data.Sequence                 as S
 import           Data.Bool                      ( bool )
-import           Test.Hspec
 import           Data.List                      ( permutations )
-import           Debug.Trace
+import qualified Data.Sequence                 as S
+import           Test.Hspec
 
 type Memory = S.Seq Int
 
-data Machine =
-  Machine
-    { pc :: Int -- program counter
-    , memory :: Memory
-    , input :: [Int]
-    , output :: [Int] -- outputs are reversed
-    } deriving Show
+data Machine
+  = Machine
+      { pc :: Int, -- program counter
+        memory :: Memory,
+        input :: [Int],
+        output :: [Int]
+      }
+  deriving (Show)
 
 mkMachine :: [Int] -> Memory -> Machine
 mkMachine inp mem = Machine { pc = 0, memory = mem, input = inp, output = [] }
 
+--
 run :: Machine -> Machine
 run m@(Machine counter mem inp outp) = case opcode of
-  -- +
+  -- (+)
   1 -> run m { pc = counter + 4, memory = set (param 3) (getP 1 + getP 2) mem }
-  -- *
+
+  -- (*)
   2 -> run m { pc = counter + 4, memory = set (param 3) (getP 1 * getP 2) mem }
 
   -- input
@@ -32,10 +34,11 @@ run m@(Machine counter mem inp outp) = case opcode of
              , input  = tail inp
              }
   -- output
-  4 -> run m { pc = counter + 2, output = getP 1 : outp }
+  4 -> run m { pc = counter + 2, output = outp ++ [getP 1] }
 
   -- Jump if true
   5 -> run m { pc = bool (getP 2) (counter + 3) (getP 1 == 0) }
+
   -- Jump if false
   6 -> run m { pc = bool (getP 2) (counter + 3) (getP 1 /= 0) }
 
@@ -47,17 +50,17 @@ run m@(Machine counter mem inp outp) = case opcode of
   8 -> run m { pc     = counter + 4
              , memory = set (param 3) (bool 0 1 (getP 1 == getP 2)) mem
              }
-
   -- halt
   99 -> m
-  o  -> error ("Invalid opcode " ++ show o ++ " at " ++ show counter)
 
+  o  -> error ("Invalid opcode " ++ show o ++ " at " ++ show counter)
  where
   get :: Int -> Int
   get = S.index mem -- get directly
 
   -- set, lazily
   set :: Int -> Int -> Memory -> Memory
+
   --set index value memry = value `seq` S.update index value memry
   set index value memry = S.update index value memry
 
@@ -65,46 +68,47 @@ run m@(Machine counter mem inp outp) = case opcode of
   param :: Int -> Int
   param i = get (counter + i)
 
-  -- the value of the positionth digit of n
-  digit :: Int -> Int -> Int
-  digit position n = n `div` (10 ^ position) `mod` 10
-
   opcode :: Int
   opcode = param 0 `mod` 100
 
-  -- the mode of the positionth parameter (0 = indirect / position mode, 1 = immediate mode)
-  mode :: Int -> Int
-  mode position = digit (position + 1) (param 0)
-
   getP position = case mode position of
-    0 -> get (param position)  -- indirect
-    1 -> param position        -- direct
+    0 -> get (param position) -- indirect
+    1 -> param position -- direct
     p ->
       error $ "Invalid parameter mode " ++ show p ++ " at pc " ++ show counter
+   where
+    -- the mode of the positionth parameter (0 = indirect / position mode, 1 = immediate mode)
+    mode :: Int -> Int
+    mode position = digit (position + 1) (param 0)
+     where
+      -- the value of the positionth digit of n
+      digit :: Int -> Int -> Int
+      digit position n = n `div` (10 ^ position) `mod` 10
 
 execute :: Memory -> [Int] -> [Int]
 execute mem inputs = output . run $ mkMachine inputs mem
 
+-- Gets the last outout, which, for Part 1, is the same as the first output
 executeAmps :: Memory -> [Int] -> Int
-executeAmps acs (a : rest) = head outLast
+executeAmps acs (phaseA : otherPhases) = last outLast
  where
-  outA    = execute acs (a : 0 : outLast)
-  outLast = foldr (\phase inputs -> execute acs (phase : inputs)) outA rest
+  outA = execute acs (phaseA : 0 : outLast)
+  outLast =
+    foldr (\phase inputs -> execute acs (phase : inputs)) outA otherPhases
 
+-- explicit version
 executeAmps' :: Memory -> [Int] -> Int
-executeAmps' acs [a, b, c, d, e] = head outE
+executeAmps' acs [phaseA, phaseB, phaseC, phaseD, phaseE] = last outE
  where
-  outA = execute acs (a : 0 : outE)
-  outB = execute acs (b : outA)
-  outC = execute acs (c : outB)
-  outD = execute acs (d : outC)
-  outE = execute acs (e : outD)
+  outA = execute acs (phaseA : 0 : outE)
+  outB = execute acs (phaseB : outA)
+  outC = execute acs (phaseC : outB)
+  outD = execute acs (phaseD : outC)
+  outE = execute acs (phaseE : outD)
 
-
---solve :: [Int] -> Int -> Int -> Int
-solve prog p0 p1 = executeAmps (S.fromList prog) <$> phases
+solve :: [Int] -> Int -> Int -> Int
+solve prog p0 p1 = maximum $ executeAmps (S.fromList prog) <$> phases
   where phases = permutations [p0 .. p1]
-
 
 main :: IO ()
 main = do
@@ -113,11 +117,22 @@ main = do
   putStr "Part 1: "
   print $ solve prog 0 4
   putStr "Part 2: "
-  -- print $ solve prog 5 9
-  print $ executeAmps' (S.fromList eg4) <$> permutations [5..9]
-  print $ executeAmps' (S.fromList eg5) <$> permutations [5..9]
+  print $ solve prog 5 9
 
-
+test :: IO ()
+test = hspec $ do
+  describe "Part 1" $ do
+    it "Example 1" $ do
+      solve eg1 0 4 `shouldBe` 43210
+    it "Example 2" $ do
+      solve eg2 0 4 `shouldBe` 54321
+    it "Example 3" $ do
+      solve eg3 0 4 `shouldBe` 65210
+  describe "Part 2" $ do
+    it "Example 1" $ do
+      solve eg4 5 9 `shouldBe` 139629729
+    it "Example 2" $ do
+      solve eg5 5 9 `shouldBe` 18216
 
 eg1 :: [Int]
 eg1 = [3, 15, 3, 16, 1002, 16, 10, 16, 1, 16, 15, 15, 4, 15, 99, 0, 0]
@@ -282,21 +297,3 @@ eg5 =
   , 0
   , 10
   ]
-
-
-{-
-test :: IO ()
-test = hspec $ do
-  describe "Part 1" $ do
-    it "Example 1" $ do
-      solve eg1 0 4 `shouldBe` 43210
-    it "Example 2" $ do
-      solve eg2 0 4 `shouldBe` 54321
-    it "Example 3" $ do
-      solve eg3 0 4 `shouldBe` 65210
-  describe "Part 2" $ do
-    it "Example 1" $ do
-      solve eg4 5 9 `shouldBe` 139629729
-    it "Example 2" $ do
-      solve eg5 5 9 `shouldBe` 18216
--}
