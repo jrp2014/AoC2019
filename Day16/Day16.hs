144b7e7ea4a3276d47ea4f2c2269563b1d8499ac
jrp2014 <jrp2014@users.noreply.github.com>
2019-12-16 23:50:35 +0000

    Day 16, Part 2...

--- Day16/Day16.hs
+++ Day16/Day16.hs
@@ -2,11 +2,20 @@ module Day16 where
 
 import           Data.Char                      ( digitToInt
                                                 , intToDigit
+                                                , isDigit
                                                 )
+import           Data.List                      ( scanl'
+                                                , unfoldr
+                                                , iterate'
+                                                )
+import           Control.Monad                  ( when
+                                                , join
+                                                )
+import           Data.Tuple                     ( swap )
 import           Test.Hspec
 
 parse :: String -> [Int]
-parse = map digitToInt . init -- drop final newline
+parse = map digitToInt . filter isDigit
 
 display :: [Int] -> String
 display = map intToDigit
@@ -19,18 +28,58 @@ fft input =
   ]
 
  where
-  lastDigit :: Int -> Int
-  lastDigit = (`mod` 10) . abs
-
   mkPattern :: Int -> [Int]
   mkPattern position = tail $ concatMap (replicate position) basePattern
 
   basePattern :: [Int]
   basePattern = cycle [0, 1, 0, -1]
 
+lastDigit :: Int -> Int
+lastDigit = (`mod` 10) . abs
+
+iterationN :: Int -> (a -> a) -> (a -> a)
+iterationN n f = (!! max 0 n) . iterate' f
+
 
 solvePt1 :: [Int] -> String
-solvePt1 input = take 8 . display . (!! 100) $ iterate fft input
+solvePt1 = take 8 . display . iterationN 100 fft
+
+solvePt2 :: String -> String
+solvePt2 input =
+  display
+    . take 8
+    . iterationN 100 comp
+    . drop offset
+    . repString 10000
+    . parse
+    $ input
+ where
+  offset = read . take 7 $ input
+  comp   = map lastDigit . reverse . cumSum . reverse
+  cumSum = tail . scanl' (+) 0
+
+
+-- A more efficient list replication concat.replicate n implementation for large n?
+-- BY RHIND PAPYRUS 'EGYPTIAN' OR 'ETHIOPIAN' MULTIPLICATION ------------------
+repString :: Int -> [a] -> [a]
+repString n s =
+  foldr
+      (\(d, x) a -> if d > 0 -- Is this power of 2 needed for the binary recomposition ?
+        then mappend a x
+        else a
+      )
+      mempty
+    $ zip
+        (unfoldr
+          (\h -> if h > 0
+            then Just $ swap (quotRem h 2) -- Binary decomposition of n
+            else Nothing
+          )
+          n
+        )
+        (iterate (join mappend) s) -- Iterative duplication ( mappend to self )
+
+
 
 main :: IO ()
 main = do
@@ -38,6 +87,10 @@ main = do
   let pinput = parse input
   putStr "Part 1: "
   print $ solvePt1 pinput
+  putStr "Part 2: "
+  let offset = (read . take 7 $ input) :: Int
+  when (offset <= length pinput `div` 2) (error "offset too small")
+  print $ solvePt2 input
 
 
 eg1 :: String
@@ -55,3 +108,12 @@ test = hspec $ do
       solvePt1 (parse "19617804207202209144916044189917") `shouldBe` "73745418"
     it "Example 4" $ do
       solvePt1 (parse "69317163492948606335995924319873") `shouldBe` "52432133"
+  describe "Part 2" $ do
+    it "Example 4" $ do
+      solvePt2 "03036732577212944063491565474664" `shouldBe` "84462026"
+    it "Example 5" $ do
+      solvePt2 "02935109699940807407585447034323" `shouldBe` "78725270"
+    it "Example 6" $ do
+      solvePt2 "03081770884921959731165446850517" `shouldBe` "53553731"
+
+
