8a8dd5b3a99c90e74e7f051f367806058c2c42c7
jrp2014 <jrp2014@users.noreply.github.com>
2019-12-18 22:22:55 +0000

    Day 17, Part 2, updated with mstksg

--- Day17/Day17.hs
+++ Day17/Day17.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE TupleSections #-}
 module Day17 where
 
 
@@ -6,6 +7,16 @@ import qualified Data.Map                      as M
 import           Data.Char                      ( chr
                                                 , ord
                                                 )
+
+import           Data.List                      ( inits
+                                                , intercalate
+                                                , unfoldr
+                                                , stripPrefix
+                                                )
+import           Data.List.Split                ( splitOn )
+import           Control.Monad                  ( guard )
+import           Control.Applicative            ( empty )
+import           Data.Foldable                  ( asum )
 
 import           IntCode
 import           Test.Hspec
@@ -59,22 +70,23 @@ solvePt1 grid = sum
   ]
 
 
-data Direction = U | D | L | R
-  deriving (Eq, Ord, Show)
+type Segment = Either Int Int
 
-type Segment = (Direction, Int)
-
+showSegment :: Segment -> String
+showSegment (Left  n) = 'L' : ',' : show n
+showSegment (Right n) = 'R' : ',' : show n
 
 start :: Grid -> Coord
 start grid = head [ k | (k, '^') <- M.toList grid ]
 
 path :: Grid -> Coord -> Coord -> [Segment]
 path grid location direction
-  | '#' == at (addCoord (turnLeft direction) location) grid = go L turnLeft
-  | '#' == at (addCoord (turnRight direction) location) grid = go R turnRight
+  | '#' == at (addCoord (turnLeft direction) location) grid = go Left turnLeft
+  | '#' == at (addCoord (turnRight direction) location) grid = go Right
+                                                                  turnRight
   | otherwise = []
  where
-  go cmd turn = (cmd, n) : path grid segmentEnd direction'
+  go cmd turn = cmd n : path grid segmentEnd direction'
    where
     direction' = turn direction
     steps      = takeWhile (\pos -> at pos grid == '#') . tail $ iterate
@@ -83,16 +95,62 @@ path grid location direction
     n          = length steps
     segmentEnd = last steps
 
-input2 :: [Int]
-input2 =
-  map ord
-    . unlines
-    $ [ "B,B,C,A,C,A,C,A,C,B"
-      , "R,10,L,12,L,12"
-      , "R,10,L,12,R,6"
-      , "R,6,R,10,R,12,R,6"
-      , "n"
-      ]
+
+findProgs :: Eq a => [a] -> [([a], [a], [a])]
+findProgs p0 = do
+  a <- validPrefix p0
+
+  let withoutA = splitOn' a p0
+  b <- case withoutA of
+    []     -> empty              -- 'A' consumed everything, whoops
+    bs : _ -> validPrefix bs
+
+  let withoutB = splitOn' b =<< withoutA
+  c <- case withoutB of
+    []     -> empty              -- 'A' and 'B' consumed everything, whoops
+    cs : _ -> validPrefix cs
+
+  let withoutC = splitOn' c =<< withoutB
+  guard $ null withoutC
+
+  pure (a, b, c)
+ where
+    -- | Get all valid prefixes
+  validPrefix = take 4 . filter (not . null) . inits
+  -- | a version of splitOn that only returns non-empty lists
+  splitOn' x = filter (not . null) . splitOn x
+
+
+-- | Given an association list of subroutines and their "label", iteratively
+-- chomp through a string replacing each occurence of the subroutine with the
+-- label.
+chomp :: Eq a => [([a], b)] -> [a] -> [b]
+chomp progs = unfoldr go
+  where go xs = asum [ (r, ) <$> stripPrefix prog xs | (prog, r) <- progs ]
+
+data Prog = A | B | C deriving (Show)
+
+solvePt2 :: Memory -> Grid -> Int
+solvePt2 prog grid = last $ execute prog (solvePt2' grid)
+
+solvePt2' :: Grid -> [Int]
+solvePt2' grid = head $ map ord . unlines . solvePt2'' <$> findProgs p
+ where
+  p :: [Segment]
+  p = path grid (start grid) (-1, 0)
+
+  solvePt2'' :: ([Segment], [Segment], [Segment]) -> [String]
+  solvePt2'' (a, b, c) = map
+    (intercalate ",")
+    [ [showProgs $ chomp [(a, A), (b, B), (c, C)] p]
+    , showSegment <$> a
+    , showSegment <$> b
+    , showSegment <$> c
+    , ["n"]
+    ]
+
+  showProgs :: [Prog] -> String
+  showProgs = tail . init . show
 
 
 main :: IO ()
@@ -105,10 +163,11 @@ main = do
   putStr "Part 1: "
   print $ solvePt1 grid
   putStr "Part 2: "
-  -- let ppath = path grid (start grid) (-1, 0)
-  -- print ppath
+  -- print $ solvePt2' grid
   let prog2 = Seq.update 0 2 prog
-  print . last $ execute prog2 input2
+  --print . last $ execute prog2 input2
+  -- print $ solvePt2' grid
+  print $ solvePt2 prog2 grid
 
 eg1 :: String
 eg1 =
