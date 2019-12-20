18c9f7c459ff2c5f6590196c206db97c05a6923b
jrp2014 <jrp2014@users.noreply.github.com>
2019-12-20 23:08:57 +0000

    Day 20, WIP

---
+++ Day20/Day20.hs
@@ -0,0 +1,85 @@
+{-# LANGUAGE ViewPatterns #-}
+module Day20 where
+
+import           Data.Char                      ( isUpper )
+import           Data.List                      ( elemIndices
+                                                , tails
+                                                , transpose
+                                                )
+import qualified Data.Map                      as Map
+import           Data.Maybe                     ( catMaybes
+                                                , maybeToList
+                                                )
+import qualified Data.Set                      as Set
+
+type Coord = (Int, Int)
+type Maze = Set.Set Coord -- passage points
+type Portals = Map.Map (Int, Int) String
+
+parse :: String -> (Maze, Portals)
+parse (lines -> rows@(transpose -> cols)) = (maze, portals) where
+  maze = Set.fromList
+    [ (x, y) | (y, row) <- zip [0 ..] rows, x <- elemIndices '.' row ]
+
+  match xy (a : b : '.' : _) | isUpper a, isUpper b = Just (xy + 2, [a, b])
+  match xy ('.' : a : b : _) | isUpper a, isUpper b = Just (xy, [a, b])
+  match _ _ = Nothing
+
+  matchAll = catMaybes . zipWith match [0 ..] . tails
+
+  portals =
+    Map.fromList
+      $  [ ((x, y), s) | (y, row) <- zip [0 ..] rows, (x, s) <- matchAll row ]
+      ++ [ ((x, y), s) | (x, col) <- zip [0 ..] cols, (y, s) <- matchAll col ]
+
+
+-- NB: This is not a universal solution ...
+invertMap :: (Ord k, Ord v) => Map.Map k v -> Map.Map v [k]
+invertMap m = Map.fromListWith (++) tuples
+  where tuples = [ (value, [key]) | (key, value) <- Map.toList m ]
+
+neighbours :: (Num a) => (a, a) -> [(a, a)]
+neighbours (x, y) = [(x - 1, y), (x, y - 1), (x, y + 1), (x + 1, y)]
+
+solvePt1 input = (start, end, next start)
+ where
+
+  (maze, portals) = parse input
+
+  portalCoords :: Map.Map String [Coord]
+  portalCoords = invertMap portals
+
+  -- Only 1 start / end Coord
+  [start]      = portalCoords Map.! "AA"
+  [end  ]      = portalCoords Map.! "ZZ"
+
+  next :: Coord -> [Coord] -- where you can get to in a single step
+  next p = filter (`Set.member` maze) (neighbours p) -- adjacent passage
+                                                     ++ do
+    portal <- maybeToList $ portals Map.!? p -- where adjacent poortals take you
+    filter (/= p) $ portalCoords Map.! portal
+
+
+eg1 :: String
+eg1 =
+  "         A           \n\
+  \         A           \n\
+  \  #######.#########  \n\
+  \  #######.........#  \n\
+  \  #######.#######.#  \n\
+  \  #######.#######.#  \n\
+  \  #######.#######.#  \n\
+  \  #####  B    ###.#  \n\
+  \BC...##  C    ###.#  \n\
+  \  ##.##       ###.#  \n\
+  \  ##...DE  F  ###.#  \n\
+  \  #####    G  ###.#  \n\
+  \  #########.#####.#  \n\
+  \DE..#######...###.#  \n\
+  \  #.#########.###.#  \n\
+  \FG..#########.....#  \n\
+  \  ###########.#####  \n\
+  \             Z       \n\
+  \             Z       "
+
+
