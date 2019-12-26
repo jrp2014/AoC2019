3957f70f505ce8b86bb6203351f6ee3a2408f07e
jrp2014 <jrp2014@users.noreply.github.com>
2019-12-26 16:30:24 +0000

    Day 24, Part 2

--- Day24/Day24.hs
+++ Day24/Day24.hs
@@ -1,184 +1,87 @@
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE TypeFamilies               #-}
+
+{-# LANGUAGE MultiWayIf    #-}
+{-# LANGUAGE TupleSections #-}
 module Day24 where
 
-import           Control.Arrow                  ( (***) )
-import           Control.Comonad                ( Comonad(..) )
-import           Control.Comonad.Representable.Store
-                                                ( Store
-                                                , StoreT(..)
-                                                , experiment
-                                                , store
-                                                )
-import           Control.Concurrent
-import           Data.Bool                      ( bool )
-import           Data.Distributive              ( Distributive(..) )
-import           Data.Functor.Compose           ( Compose(..) )
-import           Data.Functor.Identity          ( Identity(..) )
-import           Data.Functor.Rep               ( Representable(..)
-                                                , distributeRep
-                                                )
-import qualified Data.Set                      as Set
-import qualified Data.Vector                   as V
-
-import           Test.Hspec
+import qualified Data.Map                      as M
+import           Data.Set                       ( Set )
+import qualified Data.Set                      as S
 
 
--- from https://chrispenner.ca/posts/conways-game-of-life
--- ... a neat approach using comonads, but it's not much fun
--- iterating through a grid
-
-newtype VBounded a = VBounded (V.Vector a)
-  deriving (Eq, Show, Functor, Foldable)
-
-instance Distributive VBounded where
-  distribute = distributeRep
-
-instance Representable VBounded where
-  type Rep VBounded = Int
-  index (VBounded v) i = v V.! (i `mod` gridSize)
-  tabulate desc = VBounded $ V.generate gridSize desc
-
-type Grid a = Store (Compose VBounded VBounded) a
-data Coord = C !Int !Int
-
-gridSize :: Int
-gridSize = 5
-
-middle :: Coord
-middle = C (gridSize `div` 2) (gridSize `div` 2)
-
-inside :: Coord -> Bool
-inside (C x y) = 0 <= x && 0 <= y && x < gridSize && y < gridSize
-
-addCoord :: Coord -> Coord -> Coord
-addCoord (C x y) (C x' y') = C (x + x') (y + y')
-
-class    Neighbours a     where adjacents :: a -> [a]
-
-instance Neighbours Coord where
-  adjacents s = filter inside $ addCoord s <$> neighbourCoord
-
-   where
-    -- Offsets for the neighbouring 8 tiles, avoiding (0, 0) which is the cell itself
-    neighbourCoord :: [Coord]
-    neighbourCoord = [(C (-1) 0), (C 1 0), (C 0 (-1)), (C 0 1)]
-
-instance Neighbours C3    where
-  adjacents = cardinal3
-
------------------------------------------------------------------------
--- 3-dimensional recursive board coordinates
-------------------------------------------------------------------------
-
-data C3 = C3 !Int !Int !Int
-  deriving (Eq, Ord, Show)
-
-to3 :: Coord -> C3
-to3 (C y x) = C3 0 y x
-
-cardinal3, left3, right3, above3, below3 :: C3 -> [C3]
-
-cardinal3 c = concat [left3 c, right3 c, above3 c, below3 c]
-
-left3 (C3 d y x) | x == 0         = [C3 (d - 1) 2 1]
-                 | x == 3, y == 2 = [ C3 (d + 1) i 4 | i <- [0 .. 4] ]
-                 | otherwise      = [C3 d y (x - 1)]
-
-right3 (C3 d y x) | x == 4         = [C3 (d - 1) 2 3]
-                  | x == 1, y == 2 = [ C3 (d + 1) i 0 | i <- [0 .. 4] ]
-                  | otherwise      = [C3 d y (x + 1)]
-
-below3 (C3 d y x) | y == 4         = [C3 (d - 1) 3 2]
-                  | y == 1, x == 2 = [ C3 (d + 1) 0 i | i <- [0 .. 4] ]
-                  | otherwise      = [C3 d (y + 1) x]
-
-above3 (C3 d y x) | y == 0         = [C3 (d - 1) 1 2]
-                  | y == 3, x == 2 = [ C3 (d + 1) 4 i | i <- [0 .. 4] ]
-                  | otherwise      = [C3 d (y - 1) x]
+import           Control.Arrow                  (  first, second )
 
 
---mkGrid :: [Coord] -> Grid Bool
-mkGrid xs = store lookup (0 0) where lookup crd = crd `elem` xs
+type Coords = (Int, Int)
 
-type Rule = Grid Bool -> Bool
+left, right, up, down :: Coords -> Coords
+left = first pred
+right = first succ
+up = second pred
+down = second succ
 
 
-part1Rule :: Neighbours a => Rule
-part1Rule g =
-  (numNeighboursAlive == 1) || (not alive && numNeighboursAlive == 2)
+type Tile = (Int, Coords)
+
+type Grid = Set Tile
+
+size :: Int
+size = 5
+
+flatten :: [[a]] -> [((Int, Int), a)]
+flatten rows =
+  [ ((x, y), a) | (y, row) <- zip [0 ..] rows, (x, a) <- zip [0 ..] row ]
+
+firstDuplicate :: Ord a => [a] -> a
+firstDuplicate = go S.empty where
+  go seen (x : xs) | x `S.member` seen = x
+                   | otherwise         = go (S.insert x seen) xs
+
+centre :: Coords
+centre = (size `div` 2, size `div` 2)
+
+inBounds :: Coords -> Bool
+inBounds (x, y) = x >= 0 && x < size && y >= 0 && y < size
+
+neighbours :: Tile -> [Tile]
+neighbours (l, p) =
+  [ (l, p') | d <- [left, right, up, down], let p' = d p, inBounds p' ]
+
+recursiveNeighbours :: Tile -> [Tile]
+recursiveNeighbours (l, p) = do
+  (d, border) <-
+    [(left, (pred size, )), (up, (, pred size)), (right, (0, )), (down, (, 0))]
+  let p' = d p
+  if
+    | p' == centre -> [ (succ l, border i) | i <- [0 .. pred size] ]
+    | inBounds p'  -> [(l, p')]
+    | otherwise    -> [(pred l, d centre)]
+
+applyRule :: (Tile -> [Tile]) -> Grid -> Grid
+applyRule adjacents bugs =
+  keysWhere (== 1) ( n `M.restrictKeys` bugs)
+    `S.union` keysWhere (`elem` [1, 2]) ( n `M.withoutKeys` bugs)
  where
-  alive              = extract g
-  neighbours         = experiment adjacents g
-  numNeighboursAlive = length (filter id neighbours)
+  n = M.fromListWith (+) [ (p, 1) | p <- foldMap adjacents bugs ]
+  keysWhere p = M.keysSet . M.filter p
 
-step :: Rule -> Grid Bool -> Grid Bool
-step = extend
+step :: (Tile -> [Tile]) -> Grid -> [Grid]
+step = iterate . applyRule
 
-steps :: Rule -> Grid Bool -> [Grid Bool]
-steps = iterate . step
-
---experiment :: (Coord -> [Coord]) -> Grid a -> [a]
-
-render :: Grid Bool -> String
-render (StoreT (Identity (Compose g)) _) =
-  foldMap ((++ "\n") . foldMap (bool "." "#")) g
-
-at :: [Coord] -> Coord -> [Coord]
-at xs (x, y) = fmap ((+ x) *** (+ y)) xs
-
-coordLines :: [String] -> [(Coord, Char)]
-coordLines rows =
-  [ ((y, x), z) | (y, row) <- zip [0 ..] rows, (x, z) <- zip [0 ..] row ]
-
-bugCoord :: [String] -> [Coord]
-bugCoord xs = [ k | (k, '#') <- coordLines xs ]
-
-parse :: String -> Grid Bool
-parse = mkGrid . bugCoord . lines
-
-
-diversity :: Grid Bool -> Int
-diversity (StoreT (Identity (Compose g)) _) =
-  mult . foldMap (foldMap (bool [0] [1])) $ g
- where
-  mult []      = 0
-  mult (x : y) = x + 2 * mult y
-
-findDup :: Ord a => [a] -> a
-findDup = go Set.empty
- where
-  go _ [] = error "no duplicates"
-  go seen (x : xs) | Set.member x seen = x
-                   | otherwise         = go (Set.insert x seen) xs
-
-
-solvePt1 :: Grid Bool -> Int
-solvePt1 grid = findDup $ map diversity (steps part1Rule grid)
-
-loop :: (Grid Bool -> Grid Bool) -> Grid Bool -> IO ()
-loop stepper g = do
---  putStr "\ESC[2J" -- Clear terminal screen
-  putStrLn (render g)
-  threadDelay tickTime
-  loop stepper (stepper g)
+diversity :: Grid -> Int
+diversity = sum . map biodiversity . S.toList
+  where biodiversity (_, (x, y)) = 2 ^ (x + y * size)
 
 main :: IO ()
 main = do
-  putStrLn "Part 1:"
-  let start1 = parse input1
-  print $ solvePt1 start1
+  let input = flatten $ lines input1
+  let bugs  = S.fromList [ (0, p) | (p, '#') <- input ]
 
+  putStr "Part 1: "
+  print $ firstDuplicate $ diversity <$> iterate (applyRule neighbours) bugs
 
-tickTime :: Int
-tickTime = 200000
+  putStr "Part 2: "
+  print $ length $ iterate (applyRule recursiveNeighbours) bugs !! 200
 
-test :: IO ()
-test = do
-  let start1 = parse eg1
-  -- loop (step part1Rule) start1
-  print $ diversity (parse eg2)
-  print $ findDup $ map diversity (steps part1Rule start1)
 
 input1 :: String
 input1 = "###..\n\
